68586da4b06af4ca50073e4f00b6831ad91c8a5e
FelipeCiro <78991585+FelipeCiro@users.noreply.github.com>
2021-11-10 20:39:19 -0500

    Add files via upload

---
+++ ejercicio1.js
@@ -0,0 +1,6 @@
+const x = 6;
+if (5 > x && 2 === 2) {
+    document.write("Se cumplen ambas condiciones");
+} else {
+    document.write("No se cumplen ambas condiciones");
+}
---
+++ ejercicio2.js
@@ -0,0 +1,6 @@
+const x = 6;
+if (5 > x || 2 === 2) {
+    document.write("Una o ambas condiciones se cumplen");
+} else {
+    document.write("Ninguna de las condiciones se cumple");
+}
